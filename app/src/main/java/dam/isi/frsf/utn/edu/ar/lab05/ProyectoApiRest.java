caa656ca7645bb873f67426138085c914789aeda
Agustin <agustin.pane@gmail.com>
2016-10-20 20:56:00 -0300

    LAB 6

---
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/lab05/ProyectoApiRest.java
@@ -0,0 +1,35 @@
+package dam.isi.frsf.utn.edu.ar.lab05;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+import dam.isi.frsf.utn.edu.ar.lab05.modelo.Proyecto;
+
+/**
+ * Created by Agustin on 10/20/2016.
+ */
+
+public class ProyectoApiRest {
+
+    public void crearProyecto(Proyecto p){
+
+    }
+    public void borrarProyecto(Integer id){
+
+    }
+    public void actualizarProyecto(Proyecto p){
+
+    }
+    public List<Proyecto> listarProyectos(){
+        return null;
+    }
+
+    public Proyecto buscarProyecto(Integer id){
+        RestClient cliRest = new RestClient();
+        JSONObject t = cliRest.getById(1,"proyectos");
+        // transformar el objeto JSON a proyecto y retornarlo
+        return null;
+    }
+
+}
